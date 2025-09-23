1b4a555148d9dec2e1789024c1e9ea91ff53c2cc
Wyrden44 <pawel.gurlo@gmail.com>
2025-09-23 11:31:53 +0200

    Style product page

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -12,19 +12,21 @@ export default function ProductPage() {
     const { product } = useLoaderData();
 
     return (
-        <div className="product-page-card">
+        <div className="product-page">
             <button className="back-to-shop">&lt;</button>
-            <div className="product-images">
-                <img src={product.image} alt="Product Image" />
-            </div>
-            <div className="product-info">
-                <h1>{product.title}</h1>
-                <p>{product.description}</p>
-                <p id="product-price">{product.price}</p>
-            </div>
-            <div className="product-actions">
-                <input type="number" name="amount" id="product-amount" value={1} />
-                <button>Put in Cart</button>
+            <div className="product-page-card">
+                <div className="product-images">
+                    <img src={product.image} alt="Product Image" />
+                </div>
+                <div className="product-info">
+                    <h1>{product.title}</h1>
+                    <p>{product.description}</p>
+                    <p id="product-price">{product.price}$</p>
+                    <div className="product-actions">
+                        <input type="number" name="amount" id="product-amount" value={1} />
+                        <button>Put in Cart</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
