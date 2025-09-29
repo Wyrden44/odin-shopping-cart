2888e3b5327a38dbfbef588e463b799126446364
Pawel Gurlo <pawel.gurlo@gmail.com>
2025-09-29 18:46:19 +0200

    Lift product fetching up to calculate price in checkout

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,28 +1,56 @@
 import { useOutletContext } from "react-router-dom"
 import CartProduct from "./products/CartProduct";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { getProduct, useProduct } from "../fetcher";
 
 export default function Cart() {
     const [, , shoppingCart, udpateShoppingCart] = useOutletContext();
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const keys = Object.keys(shoppingCart);
+
+        async function loader() {
+            const results = await Promise.all(
+                keys.map(async (id) => {
+                    if (!products[id]) {
+                        const p = await getProduct(id);
+                        return [id, p];
+                    }
+                    return [id, products[id]];
+                })
+            );
+            setProducts(Object.fromEntries(results));
+        }
+        
+        if (keys.length > 0) {
+            loader();
+        }
+
+    }, [shoppingCart]);
+
+    console.log(shoppingCart, products)
     
     return (
         <div className="cart">
             <h1>Cart</h1>
-            <ul className="cart-products">
-                {Object.keys(shoppingCart).map((productId) => {
-                    return <li className="list-cart-product" key={productId}>
-                        <CartProduct productId={productId} amount={shoppingCart[productId]} onChange={(newAmount) => udpateShoppingCart(productId, newAmount)} />
-                    </li>
-                })}
-            </ul>
-            <div className="checkout">
-                <h2>Checkout</h2>
-                <div className="pricing">
-                    <div className="pricing-section">
-                        <p>Total:</p>
-                        <p>{Object.values(shoppingCart).reduce((prev, curr) => prev + curr, 0)}</p>
+            <div className="cart-items">
+                <ul className="cart-products">
+                    {Object.keys(shoppingCart).map((productId) => {
+                        return <li className="list-cart-product" key={productId}>
+                            <CartProduct product={products[productId]} amount={shoppingCart[productId]} onChange={(newAmount) => udpateShoppingCart(productId, newAmount)} />
+                        </li>
+                    })}
+                </ul>
+                <div className="checkout">
+                    <h2>Order Summary</h2>
+                    <div className="pricing">
+                        <div className="pricing-section">
+                            <p>Total:</p>
+                            <p>{Object.values(products).reduce((prev, product) => prev + Math.round(product.price * shoppingCart[product.id] * 100) / 100, 0)}$</p>
+                        </div>
+                        <button className="checkout-button">Checkout</button>
                     </div>
-                    <button className="checkout">Checkout</button>
                 </div>
             </div>
         </div>
--- src/components/products/CartProduct.jsx
+++ src/components/products/CartProduct.jsx
@@ -1,8 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useProduct } from "../../fetcher"
 
-export default function CartProduct({ productId, amount, onChange }) {
-    const product = useProduct(productId);
+export default function CartProduct({ product, amount, onChange }) {
     const navigate = useNavigate();
     
     if (product == null) {
@@ -15,8 +14,8 @@ export default function CartProduct({ productId, amount, onChange }) {
         <div className="cart-product">
             <img src={product.image} alt="product image" onClick={() => navigate(`../product/${product.id}`)} className="cart-product-image" />
             <div className="cart-product-info">
-                <h3>{product.title}</h3>
-                <p>{product.price}$</p>
+                    <h3>{product.title}</h3>
+                    <p>{product.price}$</p>
             </div>
             <div className="cart-product-actions">
                 <input type="number" name="cart-product-amount" value={amount} onChange={(e) => onChange(e.target.value)}/>
